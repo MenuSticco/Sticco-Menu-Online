0360bfc0fbc4b85a1336888a069b61d54a9bdae7
Lollo <gadolalorenzo@gmail.com>
2025-05-26 22:54:07 +0200

    Testing mobile menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+// function bar(button) {
+//     document.getElementById("sidebar").style.width = "250px";
+// }
+
 function bar(button) {
-    document.getElementById("sidebar").style.width = "250px";
+    const isMobile = window.innerWidth <= 768;
+    document.getElementById("sidebar").style.width = isMobile ? "85%" : "250px";
 }
+
 
 function closeSidebar() {
     document.getElementById("sidebar").style.width = "0";
