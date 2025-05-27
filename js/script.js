6e18c5eb4a0c60db27bbc687c39bab385eeccc61
Lollo <gadolalorenzo@gmail.com>
2025-05-27 15:18:28 +0200

    Fixed mobile

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 function bar(button) {
     if (window.innerWidth <= 768) {
         document.getElementById("sidebar").style.width = "85%";
+        document.body.classList.add("no-scroll");
     }
     else if (window.innerWidth <= 1024) {
         document.getElementById("sidebar").style.width = "65%";
+        document.body.classList.add("no-scroll");
     } else {
         document.getElementById("sidebar").style.width = "250px";
+        document.body.classList.add("no-scroll");
     }
 }
 
 function closeSidebar() {
-    document.getElementById("sidebar").style.width = "0"; // chiudi sidebar
+    document.getElementById("sidebar").style.width = "0"; 
+    document.body.classList.remove("no-scroll");
 }
